cd8d6af69b12c5693f3d371b01b0c60a5564475a
Soyeb <tssoyeb@gmail.com>
2022-05-19 06:35:05 +0600

    fixing error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Home from "./Home";
-import {  ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Authentication/Login/Login";
@@ -16,18 +16,21 @@ function App() {
       <Navbar></Navbar>
       <h2 className="text-3xl text-primary pt-2">Your To Do</h2>
       <div className="flex   items-center justify-center">
-       
-   
+
+
         <Routes>
           <Route path='login' element={<Login></Login>}></Route>
           <Route path="/signUp" element={<SignUp></SignUp>}></Route>
           <Route path="home" element={<RequireAuth>
             <Home></Home>
-
           </RequireAuth>}></Route>
-         
+
+          <Route path="/" element={<RequireAuth>
+            <Home></Home>
+          </RequireAuth>}></Route>
+
         </Routes>
-        <ToastContainer/>
+        <ToastContainer />
       </div>
     </div>
   );
